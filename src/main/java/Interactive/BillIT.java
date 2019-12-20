f25b8fa56637bd77ba0d5e44dbca1e653916d3d6
hiuhiu <42836577+parkouryb@users.noreply.github.com>
2019-12-20 10:14:03 +0700

    add BillIT

---
+++ src/main/java/Interactive/BillIT.java
@@ -0,0 +1,117 @@
+package Interactive;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import Object.Bill;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class BillIT {
+    public void setBill(String bill_ID, String student_ID, String bill_NAME,
+                float room_money, float water_money, float total_money,
+                Date collection_date) {
+        try {
+            sessionFactoryObj = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+
+        Session sessionObj = sessionFactoryObj.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = sessionObj.beginTransaction();
+
+            Bill bill = new Bill(bill_ID, student_ID, bill_NAME, room_money, water_money, total_money, collection_date);
+            sessionObj.save(bill);
+
+            transaction.commit();
+
+        } catch(HibernateException hibernateExeption) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("error");
+        } catch(NullPointerException np) {
+            System.out.println("not exist account to delete!");
+        } finally {
+            sessionObj.close();
+        }
+
+        if (sessionFactoryObj != null) {
+            sessionFactoryObj.close();
+        }
+    }
+
+    public void setMoney(String bill_ID, float room_money, float water_money) {
+        try {
+            sessionFactoryObj = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+
+        Session sessionObj = sessionFactoryObj.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = sessionObj.beginTransaction();
+
+            Bill bill = (Bill) sessionObj.get(Bill.class.getName(), bill_ID);
+            bill.setRoom_money(room_money);
+            bill.setWater_money(water_money);
+
+            transaction.commit();
+
+        } catch(HibernateException hibernateExeption) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("error");
+        } catch(NullPointerException np) {
+            System.out.println("not exist account to delete!");
+        } finally {
+            sessionObj.close();
+        }
+
+        if (sessionFactoryObj != null) {
+            sessionFactoryObj.close();
+        }
+    }
+
+    public void setCollection_Date(String bill_ID, Date collection_date) {
+        try {
+            sessionFactoryObj = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+
+        Session sessionObj = sessionFactoryObj.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = sessionObj.beginTransaction();
+
+            Bill bill = (Bill) sessionObj.get(Bill.class.getName(), bill_ID);
+            bill.setCollection_date(collection_date);
+
+            transaction.commit();
+
+        } catch(HibernateException hibernateExeption) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("error");
+        } catch(NullPointerException np) {
+            System.out.println("not exist account to delete!");
+        } finally {
+            sessionObj.close();
+        }
+
+        if (sessionFactoryObj != null) {
+            sessionFactoryObj.close();
+        }
+    }
+}
