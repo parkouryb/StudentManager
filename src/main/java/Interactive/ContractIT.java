836a3801e873267c000726b1b61e17cea060848d
hiuhiu <42836577+parkouryb@users.noreply.github.com>
2019-12-20 10:35:08 +0700

    add

---
+++ src/main/java/Interactive/ContractIT.java
@@ -0,0 +1,98 @@
+package Interactive;
+
+import java.util.Date;
+import Object.Contract;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ContractIT {
+    public void addContract(String contract_ID, Date day_in, Date day_out, Date creation_date) {
+        try {
+            sessionFactoryObj = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+
+        Session sessionObj = sessionFactoryObj.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = sessionObj.beginTransaction();
+
+            Contract contract = new Contract(contract_ID, day_in, day_out, creation_date);
+            sessionObj.save(contract);
+
+            transaction.commit();
+        } catch(HibernateException hibernateExeption) {
+            System.out.println("error\n");
+        } finally {
+            sessionObj.close();
+        }
+
+        if (sessionFactoryObj != null) {
+            sessionFactoryObj.close();
+        }
+    }
+
+    public void deleteContract(String contract_ID) {
+        try {
+            sessionFactoryObj = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+
+        Session sessionObj = sessionFactoryObj.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = sessionObj.beginTransaction();
+
+            Contract contract = (Contract) sessionObj.get(Contract.class.getName(), contract_ID);
+            sessionObj.flush();
+            sessionObj.delete(contract);
+            sessionObj.flush();
+            transaction.commit();
+
+        } catch(HibernateException hibernateExeption) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("error");
+        } catch(NullPointerException np) {
+            // to do
+        } finally {
+            sessionObj.close();
+        }
+
+        if (sessionFactoryObj != null) {
+            sessionFactoryObj.close();
+        }
+    }
+
+    public Contract searchContract(String contract_ID) {
+        try {
+            sessionFactoryObj = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+
+        Session sessionObj = sessionFactoryObj.openSession();
+        Contract contract = null;
+        try {
+            contract = (Contract) sessionObj.get(Contract.class.getName(), contract_ID);
+
+        } catch(HibernateException hibernateExeption) {
+            System.out.println("error\n");
+        }finally {
+            sessionObj.close();
+        }
+
+        if (sessionFactoryObj != null) {
+            sessionFactoryObj.close();
+        }
+        return contract;
+
+    }
+}
